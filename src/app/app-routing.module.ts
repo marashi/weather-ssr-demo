576d03dbd607e1a98cd9a340248a79aa1cc1f311
Matt Marashi <matt@marashi.dev>
2023-06-26 20:58:55 -0400

    feat: implement weather component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WeatherComponent } from './weather/weather.component';
 
-const routes: Routes = [{ path: 'weather', component: WeatherComponent }];
+const routes: Routes = [
+  { path: 'weather', component: WeatherComponent },
+  { path: '', redirectTo: '/weather', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,19 +1,34 @@
 import { Component } from '@angular/core';
 import { WeatherService } from '../weather.service';
+import { BehaviorSubject, EMPTY, Observable, switchMap } from 'rxjs';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent {
-  weatherData: any;
+  public isMenuOpen = false;
+  public weatherData$: Observable<any> = EMPTY;
+  private selectedCity$: BehaviorSubject<string> = new BehaviorSubject<string>(
+    'toronto'
+  );
 
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
-    this.weatherService.getWeatherData('Toronto').subscribe((data) => {
-      console.log({ data });
-      this.weatherData = data;
-    });
+    this.weatherData$ = this.selectedCity$.pipe(
+      switchMap((city: string) => this.weatherService.getWeatherData(city))
+    );
+  }
+
+  toggleDropdown() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  onSelectCity(event: any) {
+    const city = event.target.value;
+    if (city) {
+      this.selectedCity$.next(city);
+    }
   }
 }
